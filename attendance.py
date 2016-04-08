1e1de7df8905d605e0ef513c5d7a5c441ea3e533
pugly <bonezep3@gmail.com>
2016-04-08 01:13:09 -0400

    Reneeeeeee
    I dont know how to run this so I am not sure if it has errors. Also, instead of typing L or l I made it compare the entry time to a preset period start time so that the late mark is automated.

--- attendance.py
+++ attendance.py
@@ -3,6 +3,8 @@ __author__ = 'denrico'
 import time as t
 import shelve
 
+period_start_H = 9
+period_start_M = 0
 s = shelve.open('my_students', writeback=True)
 
 while True:
@@ -16,8 +18,18 @@ while True:
         print('ID not found; scan again.')
 
     else:
-        print(s[entry]['name'] + ' scanned in at ' + t.strftime('%H:%M\n', t.localtime()))
-        s[entry]['attendance'].append(t.time())
+        if int(t.strftime('%H')) <= period_start_H:
+            if int(t.strftime('%M')) <= period_start_H:
+                print(s[entry]['name'] + ' scanned in at ' + t.strftime('%H:%M\n', t.localtime()))
+                s[entry]['attendance'].append(t.time())
+            else:
+                print(s[entry]['name'] + ' scanned in at ' + t.strftime('%H:%M\n', t.localtime())+'LATE')
+                s[entry]['attendance'].append(t.time())
+                s[entry]['lates'].append(t.time())
+        else:
+            print(s[entry]['name'] + ' scanned in at ' + t.strftime('%H:%M\n', t.localtime())+'LATE')
+            s[entry]['attendance'].append(t.time())
+            s[entry]['lates'].append(t.time())
 
 
 
